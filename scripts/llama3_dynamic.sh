ee19197c60bc4cc8d1070275b4493e3f0dd3d170
Yi-Lin Sung <corumlouis123@gmail.com>
2024-11-13 14:29:23 -0500

    add script for dynamic bitmap

---
+++ scripts/llama3_dynamic.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=$1 python llama.py \
+    meta-llama/Meta-Llama-3-8B-Instruct \
+    c4 \
+    --wbits 4 \
+    --true-sequential \
+    --new-eval \
+    --wbits-yaml configs/qkvo4_mlp16.yaml
+
+# use --save_in_16bits to save the model in 16-bit format
